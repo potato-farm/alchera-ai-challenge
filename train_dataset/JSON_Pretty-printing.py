c7594e19f40db52ca4bc8bf5a7ceddbf51018036
Rachel Lee <rachel318318@gmail.com>
2021-11-09 13:20:08 +0000

    fix: run as main

--- train_dataset/JSON_Pretty-printing.py
+++ train_dataset/JSON_Pretty-printing.py
@@ -3,21 +3,26 @@ import json
 '''
 Make JSON to be Prettier and Readable
 '''
-# Train.json
-file_path = "./densepose_coco_2014_train.json"
-json_data = {}
-with open(file_path, "r") as json_file:
-    json_data = json.load(json_file)
+def main():
+    # Train.json
+    file_path = "./densepose_coco_2014_train.json"
+    json_data = {}
+    with open(file_path, "r") as json_file:
+        json_data = json.load(json_file)
 
-with open(file_path, 'w') as outfile:
-    json.dump(json_data, outfile, indent = 4, sort_keys = True)
+    with open(file_path, 'w') as outfile:
+        json.dump(json_data, outfile, indent = 4, sort_keys = True)
 
 
-# Val.json
-file_path = "./densepose_coco_2014_minival.json"
-json_data = {}
-with open(file_path, "r") as json_file:
-    json_data = json.load(json_file)
+    # Val.json
+    file_path = "./densepose_coco_2014_minival.json"
+    json_data = {}
+    with open(file_path, "r") as json_file:
+        json_data = json.load(json_file)
 
-with open(file_path, 'w') as outfile:
-    json.dump(json_data, outfile, indent = 4, sort_keys = True)
+    with open(file_path, 'w') as outfile:
+        json.dump(json_data, outfile, indent = 4, sort_keys = True)
+
+
+if __name__ == "__main__":
+    main()
