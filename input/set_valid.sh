25ea0b5f462f9f589251b6c2e5a1503ef3cbb644
SeyoungKim <50132040+Seyoung9304@users.noreply.github.com>
2021-11-11 11:17:49 +0900

    Update set_valid.sh
    add rm .zip

--- input/set_valid.sh
+++ input/set_valid.sh
@@ -5,3 +5,4 @@ cd original
 find . -type f -name "*.jpg" -exec mv {} ../val2014 \;
 find . -type f -name "*.png" -exec mv {} ../val_mask \;
 rm -r original
+rm Inbody_Segmentation_testset_Participant.zip
