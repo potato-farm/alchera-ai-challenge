9cb543f93e8c80a4e64ca03dc577efef82837634
seung-sss <sjun804@gmail.com>
2021-11-12 10:34:10 +0000

    modify set_valid.sh

--- input/set_valid.sh
+++ input/set_valid.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 unzip Inbody_Segmentation_testset_Participant.zip -d ./original
+mkdir -p val_mask
+mkdir -p val2014
 cd original
 find . -type f -name "*.jpg" -exec mv {} ../val2014 \;
 find . -type f -name "*.png" -exec mv {} ../val_mask \;
+cd ..
 rm -r original
 rm Inbody_Segmentation_testset_Participant.zip
