ebb89b00c7b1b7217f78efeb3008f5afe60e224c
seung-sss <sjun804@gmail.com>
2021-11-12 10:28:00 +0000

    delete input

--- input/set_valid.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-unzip Inbody_Segmentation_testset_Participant.zip -d ./original
-cd original
-find . -type f -name "*.jpg" -exec mv {} ../val2014 \;
-find . -type f -name "*.png" -exec mv {} ../val_mask \;
-rm -r original
-rm Inbody_Segmentation_testset_Participant.zip
