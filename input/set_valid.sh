fda051c50cd51911253a1bca4b123b97a3a16918
Rachel Lee <rachel318318@gmail.com>
2021-11-16 06:40:30 +0000

    fix: updated val images

--- input/set_valid.sh
+++ input/set_valid.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-unzip Inbody_Segmentation_testset_Participant.zip -d ./original
+mkdir -p original
+unzip Inbody_Segmentation_testset_Participant_pixelchange_gray-smooth.zip -d ./original
 mkdir -p val_mask
 mkdir -p val2014
 cd original
@@ -8,4 +9,4 @@ find . -type f -name "*.jpg" -exec mv {} ../val2014 \;
 find . -type f -name "*.png" -exec mv {} ../val_mask \;
 cd ..
 rm -r original
-rm Inbody_Segmentation_testset_Participant.zip
+rm Inbody_Segmentation_testset_Participant_pixelchange_gray-smooth.zip
