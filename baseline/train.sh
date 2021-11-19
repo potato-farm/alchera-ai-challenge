1f46ae920bc092f886a7103f7082c6883811ae9d
Rachel Lee <rachel318318@gmail.com>
2021-11-19 01:57:29 +0000

    chore: final commit

---
+++ baseline/train.sh
@@ -0,0 +1,5 @@
+#To run multiple models
+python train.py --dir deeplabv3+_cspresnet50
+python train.py --dir deeplabv3+_cspresnext50
+python train.py --dir deeplabv3+_effb4
+#python train.py --dir deeplabv3+_resnext50
--- input/set_valid.sh
+++ input/set_valid.sh
@@ -9,4 +9,4 @@ find . -type f -name "*.jpg" -exec mv {} ../val2014 \;
 find . -type f -name "*.png" -exec mv {} ../val_mask \;
 cd ..
 rm -r original
-rm Inbody_Segmentation_testset_Participant_pixelchange_gray-smooth.zip
+#rm Inbody_Segmentation_testset_Participant_pixelchange_gray-smooth.zip
